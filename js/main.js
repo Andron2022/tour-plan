f2ef8a87002ac4967a4769d84f65d58a3a16682d
Andron22 <ashevcab@gmail.com>
2022-06-17 18:26:39 +0500

    create: added reviews block and fixed hotelslider

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  // direction: "vertical",
+const hotelSlired = new Swiper(".hotel-slider", {
   loop: true,
 
-  // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-
-  // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
+});
+const reviewsSlider = new Swiper(".reviews-slider", {
+  loop: true,
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
 });
